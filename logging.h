65c1be49d819f32360d46cd3ec96be78c6a91bb6
Franz-Josef Haider <f_haider@gmx.at>
2014-11-19 13:44:14 +0100

    only use printk if THOR_DEBUG is set (in logging.h) because we don't want to reveal ourself in dmesg in a hacking scenario

--- logging.h
+++ logging.h
@@ -8,13 +8,15 @@
 #if THOR_DEBUG
 #   define LOG_DEBUG(format, args...) \
         printk(KERN_DEBUG LOG_TAG format "\n", ##args);
+#   define LOG_ERROR(format, args...) \
+        printk(KERN_ERR LOG_TAG format "\n", ##args);
+#   define LOG_INFO(format, args...) \
+        printk(KERN_INFO LOG_TAG format "\n", ##args);
 #else
 #   define LOG_DEBUG(format, args...) do {} while(0);
+#   define LOG_ERROR(format, args...) do {} while(0);
+#   define LOG_INFO(format, args...) do {} while(0);
 #endif
-#define LOG_ERROR(format, args...) \
-    printk(KERN_ERR LOG_TAG format "\n", ##args);
-#define LOG_INFO(format, args...) \
-    printk(KERN_INFO LOG_TAG format "\n", ##args);
 
 #endif /* __LOGGING_H__ */
 
