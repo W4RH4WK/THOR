bd27fdd4633ba8ba2c10cc8060e09bfbafc4a943
Frajo Haider <f_haider@gmx.at>
2014-12-09 21:03:54 +0100

    continue work for ARM platforms with strict memory protection (CONFIG_STRICT_MEMORY_RWX)

--- helper.c
+++ helper.c
@@ -4,9 +4,156 @@
 
 #if defined(CONFIG_ARM)
 # include <asm/cacheflush.h>
-# if defined(CONFIG_STRICT_MEMORY_RWX)
-#  include <asm/mmu_writeable.h>
-# endif
+#endif
+
+#if defined(CONFIG_ARM) && defined(CONFIG_STRICT_MEMORY_RWX)
+#include <linux/sched.h>
+#include <asm/tlbflush.h>
+#include <asm/smp_plat.h>
+
+#undef dsb
+#define dsb(option) __asm__ __volatile__ ("dsb " #option : : : "memory")
+#define possible_tlb_flags      (v4_possible_flags | \
+                                 v4wbi_possible_flags | \
+                                 fr_possible_flags | \
+                                 v4wb_possible_flags | \
+                                 fa_possible_flags | \
+                                 v6wbi_possible_flags | \
+                                 v7wbi_possible_flags)
+#define always_tlb_flags        (v4_always_flags & \
+                                 v4wbi_always_flags & \
+                                 fr_always_flags & \
+                                 v4wb_always_flags & \
+                                 fa_always_flags & \
+                                 v6wbi_always_flags & \
+                                 v7wbi_always_flags)
+
+#define tlb_flag(f)     ((always_tlb_flags & (f)) || (__tlb_flag & possible_tlb_flags & (f)))
+
+#define tlb_op(f, regs, arg)    __tlb_op(f, "p15, 0, %0, " regs, arg)
+
+static struct {
+    pmd_t *pmd_to_flush;
+    pmd_t *pmd;
+    unsigned long addr;
+    pmd_t saved_pmd;
+    bool made_writeable;
+} mem_unprotect;
+
+struct tlb_args {
+        struct vm_area_struct *ta_vma;
+        unsigned long ta_start;
+        unsigned long ta_end;
+};
+
+static inline void __local_flush_tlb_kernel_page(unsigned long kaddr)
+{
+    const int zero = 0;
+    const unsigned int __tlb_flag = __cpu_tlb_flags;
+
+    tlb_op(TLB_V4_U_PAGE, "c8, c7, 1", kaddr);
+    tlb_op(TLB_V4_D_PAGE, "c8, c6, 1", kaddr);
+    tlb_op(TLB_V4_I_PAGE, "c8, c5, 1", kaddr);
+    if (!tlb_flag(TLB_V4_I_PAGE) && tlb_flag(TLB_V4_I_FULL))
+            asm("mcr p15, 0, %0, c8, c5, 0" : : "r" (zero) : "cc");
+
+    tlb_op(TLB_V6_U_PAGE, "c8, c7, 1", kaddr);
+    tlb_op(TLB_V6_D_PAGE, "c8, c6, 1", kaddr);
+    tlb_op(TLB_V6_I_PAGE, "c8, c5, 1", kaddr);
+}
+
+static inline void __flush_tlb_kernel_page(unsigned long kaddr)
+{
+    const unsigned int __tlb_flag = __cpu_tlb_flags;
+
+    kaddr &= PAGE_MASK;
+
+    if (tlb_flag(TLB_WB))
+        dsb(ishst);
+
+    __local_flush_tlb_kernel_page(kaddr);
+    tlb_op(TLB_V7_UIS_PAGE, "c8, c3, 1", kaddr);
+
+    if (tlb_flag(TLB_BARRIER)) {
+        dsb(ish);
+        isb();
+    }
+}
+
+static inline void ipi_flush_tlb_kernel_page(void *arg)
+{
+    struct tlb_args *ta = (struct tlb_args *)arg;
+
+    local_flush_tlb_kernel_page(ta->ta_start);
+}
+
+void flush_tlb_kernel_page(unsigned long kaddr)
+{
+    if (tlb_ops_need_broadcast()) {
+        struct tlb_args ta;
+        ta.ta_start = kaddr;
+        on_each_cpu(ipi_flush_tlb_kernel_page, &ta, 1);
+    } else
+        __flush_tlb_kernel_page(kaddr);
+    /* XXX */
+    /*broadcast_tlb_a15_erratum();*/
+}
+
+void mem_text_address_writeable(unsigned long addr)
+{
+    struct task_struct *tsk = current;
+    struct mm_struct *mm = tsk->active_mm;
+    pgd_t *pgd = pgd_offset(mm, addr);
+    pud_t *pud = pud_offset(pgd, addr);
+
+    mem_unprotect.made_writeable = 0;
+
+/*  removed because we actually want to write to non text sections */
+/*
+    if ((addr < (unsigned long)RX_AREA_START) ||
+        (addr >= (unsigned long)RX_AREA_END))
+        return;
+*/
+
+    mem_unprotect.pmd = pmd_offset(pud, addr);
+    mem_unprotect.pmd_to_flush = mem_unprotect.pmd;
+    mem_unprotect.addr = addr & PAGE_MASK;
+
+    if (addr & SECTION_SIZE)
+            mem_unprotect.pmd++;
+
+    mem_unprotect.saved_pmd = *mem_unprotect.pmd;
+    if ((mem_unprotect.saved_pmd & PMD_TYPE_MASK) != PMD_TYPE_SECT)
+        return;
+
+    *mem_unprotect.pmd &= ~PMD_SECT_APX;
+
+    flush_pmd_entry(mem_unprotect.pmd_to_flush);
+    flush_tlb_kernel_page(mem_unprotect.addr);
+    mem_unprotect.made_writeable = 1;
+}
+
+void mem_text_address_restore(void)
+{
+    if (mem_unprotect.made_writeable) {
+        *mem_unprotect.pmd = mem_unprotect.saved_pmd;
+        flush_pmd_entry(mem_unprotect.pmd_to_flush);
+        flush_tlb_kernel_page(mem_unprotect.addr);
+    }
+}
+
+static DEFINE_SPINLOCK(mem_text_writeable_lock);
+
+void mem_text_writeable_spinlock(unsigned long *flags)
+{
+	spin_lock_irqsave(&mem_text_writeable_lock, *flags);
+}
+
+void mem_text_writeable_spinunlock(unsigned long *flags)
+{
+	spin_unlock_irqrestore(&mem_text_writeable_lock, *flags);
+}
+
 #endif
 
 #if defined(CONFIG_X86)
@@ -41,13 +188,14 @@ void write_no_prot(void *addr, void *data, int len)
     set_addr_ro(addr);
 #elif defined(CONFIG_ARM)
 # if defined(CONFIG_STRICT_MEMORY_RWX)
-    unsigned long *target_arm = (unsigned long*)addr;
-    unsigned long *code_arm = (unsigned long*)data;
-    unsigned int i;
-    for(i=0;i<len;i++)
-    {
-        mem_text_write_kernel_word(target_arm + i, *(code_arm + i));
-    }
+    unsigned long flags;
+    mem_text_writeable_spinlock(&flags);
+    // TODO: mem_text_address_writeable on actual len
+    mem_text_address_writeable((unsigned long)addr);
+    memcpy(addr, data, len);
+    cacheflush(addr, len);
+    mem_text_address_restore();
+    mem_text_writeable_spinunlock(&flags);
 # else
     memcpy(addr, data, len);
     cacheflush(addr, len);
